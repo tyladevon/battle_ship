291b7b46544e3e70df084e5e51b014704a6b50b1
David Lopez <davidclopez1@live.com>
2019-08-20 15:31:54 -0600

    Add test for coordinate
    Co-authored-by: Tyla Devon <tyladevon@gmail.com>

--- tests/cell_test.rb
+++ tests/cell_test.rb
@@ -7,12 +7,14 @@ require 'minitest/pride'
 class TestCell < Minitest::Test
 
   def setup
-    @cell = Cell.new
+    @cell = Cell.new("B4")
   end
 
   def test_it_exists
     assert_instance_of Cell, @cell
   end
 
-  
+  def test_for_coordinate
+    assert_equal "B4", @cell.coordinate
+  end
 end

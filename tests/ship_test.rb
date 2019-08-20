b507d58502e5106e777f7cd767a451b086e881aa
Tyla Devon <tyladevon@gmail.com>
2019-08-19 20:31:52 -0600

    Add test for ship name
    Co-authored-by: David Lopez <ldavid.x.lopezl@gmail.com>

--- tests/ship_test.rb
+++ tests/ship_test.rb
@@ -5,12 +5,15 @@ require 'minitest/pride'
 
 class TestShip < Minitest::Test
   def setup
-    @new_ship = Ship.new
+    @new_ship = Ship.new("Ship")
   end
 
   def test_ship_exists
     assert_instance_of Ship, @new_ship
   end
 
+  def test_it_has_a_name
+    assert_equal "Ship", @new_ship.name
+  end
 
 end

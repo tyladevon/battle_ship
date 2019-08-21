da89c46bf746cb0c3bef270cbac5041b67a631f2
Tyla Devon <tyladevon@gmail.com>
2019-08-21 14:39:06 -0600

    Add test for valid_coordinate
    Co-authored-by: David Lopez <ldavid.x.lopezl@gmail.com>

--- tests/board_test.rb
+++ tests/board_test.rb
@@ -13,8 +13,20 @@ class TestBoard < Minitest::Test
     @cell_2 = Cell.new("A1")
     @board = Board.new
   end
-
   def test_if_exists
     assert_instance_of Board, @board
+  end
+
+  def test_for_cells_indirectly
+    assert_equal false, @board.cells["A1"].fired_upon?
+    @board.cells["B3"].fire_upon
+    assert_equal true, @board.cells["B3"].fired_upon?
+    @board.cells["D2"].fire_upon
+    assert_equal true, @board.cells["D2"].fired_upon?
+  end
+
+  def test_valid_coordinate
+    assert_equal true, @board.valid_coordinate?("A1")
+    assert_equal false, @board.valid_coordinate?("jty")
   end
 end

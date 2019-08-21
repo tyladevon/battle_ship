6f927907c5ffea83c4e328423605a5a2c7621f36
Tyla Devon <tyladevon@gmail.com>
2019-08-20 20:28:18 -0600

    Add fired_upon method
    Co-authored-by: David Lopez <ldavid.x.lopezl@gmail.com>

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,10 +1,11 @@
 class Cell
-  attr_reader :coordinate, :ship, :placement 
+  attr_reader :coordinate, :ship, :placement
 
   def initialize(coordinate)
     @coordinate = coordinate
     @ship
     @placement = Hash.new(0)
+    @fired_upon = false
   end
 
   def empty?
@@ -14,5 +15,9 @@ class Cell
   def place_ship(ship)
     @ship = ship
     @placement[@coordinate] = @ship
+  end
+
+  def fired_upon?
+    @fired_upon
   end
 end

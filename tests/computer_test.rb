59fdf2998747a8bc425a0d4f39d96dcb537be65f
Tyla Devon <tyladevon@gmail.com>
2019-08-27 14:45:31 -0600

    Add test for place ship
    Co-authored-by: David Lopez <ldavid.x.lopezl@gmail.com>

--- tests/computer_test.rb
+++ tests/computer_test.rb
@@ -9,6 +9,8 @@ require 'minitest/pride'
 class ComputerTest < Minitest::Test
   def setup
     @computer_board = Board.new
+    @computer_cruiser = Ship.new("Cruiser", 3)
+    @computer_submarine = Ship.new("Submarine", 2)
     @computer = Computer.new(@computer_board)
   end
 
@@ -19,4 +21,17 @@ class ComputerTest < Minitest::Test
   def test_computer_can_take_board
     assert_instance_of Board, @computer.board
   end
+
+  def test_if_placed_ship
+    @computer.calculate_coordinates(@computer_cruiser)
+    x = @computer.board.cells.values.count do |cell|
+      cell.ship != nil
+    end
+    assert_equal 3, x
+  end
+
+  # def test_if_computer_fails
+  #   assert_equal "Computer couldn't place a ship. Please start again.", @computer.calculate_coordinates(@computer_cruiser)
+  # end
+
 end
